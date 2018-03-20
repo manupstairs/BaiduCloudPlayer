17eee7c0fa00ce4de2dd46ac5d66097669bd0f50
manupstairs <manupstairs@163.com>
2018-03-20 22:56:08 +0800

    add loginview

---
+++ BaiduCloudPlayer/BaiduCloudPlayer/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace BaiduCloudPlayer
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string BaiduClientId { get; set; } = "CuOLkaVfoz1zGsqFKDgfvI0h";
+        private string RedirectUri { get; set; } = "oob";
+        private string Scope { get; set; } = "netdisk";
+
+        public string LoginUri
+        {
+            get
+            {
+                return string.Format("https://openapi.baidu.com/oauth/2.0/authorize?response_type=token&client_id={0}&redirect_uri={1}&scope={2}",
+                    BaiduClientId, RedirectUri, Scope);
+            }
+        }
+
+        //private Uri content;
+
+        //public Uri Content
+        //{
+        //    get { return content; }
+        //    set { Set(ref content, value); }
+        //}
+
+
+        public ICommand LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            this.LoginCommand = new RelayCommand<NavigationEventArgs>(LoginAction);
+        }
+
+        private void LoginAction(NavigationEventArgs e)
+        {
+            if (e.Uri.AbsolutePath.Contains("login_success"))
+            {
+                string[] parm = e.Uri.Fragment.Split('#')[1].Split('&');
+                foreach (string p in parm)
+                {
+                    string[] sub = p.Split('=');
+                    // Get each parameter
+                    switch (sub[0])
+                    {
+                        //case "access_token":
+                        //    MainWindow.totalData.Access_Token = sub[1];
+                        //    break;
+                        //case "expires_in":
+                        //    MainWindow.totalData.Expires_In = sub[1];
+                        //    break;
+                        //case "session_secret":
+                        //    MainWindow.totalData.Session_Secret = sub[1];
+                        //    break;
+                        //case "session_key":
+                        //    MainWindow.totalData.Session_Key = sub[1];
+                        //    break;
+                        //case "scope":
+                        //    MainWindow.totalData.Scope = sub[1];
+                        //    break;
+                    }
+                }
+            }
+        }
+    }
+}
